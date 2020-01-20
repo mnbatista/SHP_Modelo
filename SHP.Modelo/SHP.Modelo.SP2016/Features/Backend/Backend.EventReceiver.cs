9101427f1cfb6fa2fa53365d1961ff72b124723d
Maycon de Novaes Batista <maycon_novaes@hotmail.com>
2020-01-20 17:18:57 -0200

    FeatureUpgrade

--- SHP.Modelo/SHP.Modelo.SP2016/Features/Backend/Backend.EventReceiver.cs
+++ SHP.Modelo/SHP.Modelo.SP2016/Features/Backend/Backend.EventReceiver.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Security.Permissions;
 using Microsoft.SharePoint;
@@ -47,5 +48,66 @@ namespace SHP.Modelo.SP2016.Features.Backend
         //public override void FeatureUpgrading(SPFeatureReceiverProperties properties, string upgradeActionName, System.Collections.Generic.IDictionary<string, string> parameters)
         //{
         //}
+
+        public override void FeatureUpgrading(SPFeatureReceiverProperties properties, string upgradeActionName, IDictionary<string, string> parameters)
+        {
+            var parentWeb = (SPWeb)properties.Feature.Parent;
+            if (parentWeb == null) return;
+
+            SPSecurity.RunWithElevatedPrivileges(() =>
+            {
+                using (var elevatedSite = new SPSite(parentWeb.ID))
+                {
+                    using (var elevatedWeb = elevatedSite.OpenWeb(elevatedSite.ID))
+                    {
+                        try
+                        {
+                            switch (upgradeActionName)
+                            {
+                                case "V2Update":
+                                    parentWeb.AllowUnsafeUpdates = true;
+
+                                    foreach (string key in parameters.Keys)
+                                    {
+                                        // Iterate and split each content type, field value.
+                                        string value = parameters[key];
+
+                                        string[] parts = value.Split(',');
+
+                                        SPContentTypeId contentTypeId = new SPContentTypeId(parts[0]);
+                                        Guid fieldId = new Guid(parts[1]);
+
+                                        SPField field = parentWeb.Fields[fieldId];
+                                        SPFieldLink fieldLink = new SPFieldLink(field);
+                                        SPContentType contentType = parentWeb.ContentTypes[contentTypeId];
+
+                                        //Logger.Information("Adding field \"{0}\" to content type \"{1}\".", field.Title, contentType.Name);
+
+                                        contentType.FieldLinks.Delete(fieldId);
+
+                                        contentType.Update(true);
+
+                                        contentType.FieldLinks.Add(fieldLink);
+
+                                        contentType.Update(true);
+                                    }
+                                    parentWeb.AllowUnsafeUpdates = false;
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            // log error
+                            //ogger.Unexpected("GCS.NovosCanais__BrandingEventReceiver.FeatureUpgrading", ex);
+                            throw;
+                        }
+                    }
+                }
+            });
+
+
+        }
     }
 }
--- SHP.Modelo/SHP.Modelo.SP2016/Features/Frontend/Frontend.feature
+++ SHP.Modelo/SHP.Modelo.SP2016/Features/Frontend/Frontend.feature
@@ -1,8 +1,2 @@
 ﻿<?xml version="1.0" encoding="utf-8"?>
-<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="4591ae7a-e906-4b4d-a982-ab77978fd971" description="Esse recurso instala os arquivos html, js, css e demais assets necessários para a implantação do Modelo." featureId="4591ae7a-e906-4b4d-a982-ab77978fd971" imageUrl="" receiverAssembly="$SharePoint.Project.AssemblyFullName$" receiverClass="$SharePoint.Type.b70863a2-e7b8-4042-a0c3-007b2514b418.FullName$" solutionId="00000000-0000-0000-0000-000000000000" title="Modelo - Frontend" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
-  <projectItems>
-    <projectItemReference itemId="7f5ab497-cc52-4b28-b752-67e7cadcf298" />
-    <projectItemReference itemId="4eae1dfe-9db1-4c63-b628-465bdbb51ad2" />
-    <projectItemReference itemId="87a4c520-c367-43d5-9dc2-948f05625da7" />
-  </projectItems>
-</feature>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="4591ae7a-e906-4b4d-a982-ab77978fd971" description="Esse recurso instala os arquivos html, js, css e demais assets necessários para a implantação do Modelo." featureId="4591ae7a-e906-4b4d-a982-ab77978fd971" imageUrl="" receiverAssembly="$SharePoint.Project.AssemblyFullName$" receiverClass="$SharePoint.Type.b70863a2-e7b8-4042-a0c3-007b2514b418.FullName$" solutionId="00000000-0000-0000-0000-000000000000" title="Modelo - Frontend" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel" />
